632eb908121d46394054c65cab0c17629389edd3
stav rockah <stav.r.24@gmail.com>
2018-11-21 05:50:24 +0200

    fix issues & add handle cars positions

--- prolog-src/constraints.pl
+++ prolog-src/constraints.pl
@@ -29,7 +29,7 @@ valid_position(CUR_POS_X, CUR_POS_Y, POS_X, POS_Y) :-
     ((CUR_POS_X-1 > 2, MIN_X is CUR_POS_X-1); MIN_X is 2),
     ((CUR_POS_X+1 < 10, MAX_X is CUR_POS_X+1); MAX_X is 10),
     ((CUR_POS_Y-10 > 0, MIN_Y is CUR_POS_Y-10); MIN_Y is 0),
-    ((CUR_POS_Y+100 < 6946, MAX_Y is CUR_PO_SY+100); MAX_Y is 6946),
+    ((CUR_POS_Y+100 < 6946, MAX_Y is CUR_POS_Y+100); MAX_Y is 6946),
     range(POS_X,MIN_X,MAX_X), 	
 	range(POS_Y,MIN_Y,MAX_Y).
 
@@ -38,7 +38,7 @@ displacement(CUR_POS_X, CUR_POS_Y, NEXT_POS_X, NEXT_POS_Y, DISP_X, DISP_Y) :-
     DISP_X is NEXT_POS_X - CUR_POS_X,
     DISP_Y is NEXT_POS_Y - CUR_POS_Y.
 
-range(Low, Low, High).
+range(Low, Low, _).
 range(Out,Low,High) :- NewLow is Low+1, NewLow =< High, range(Out, NewLow, High).
 
 % average velocity between positions
@@ -82,9 +82,17 @@ valid_jerk(CUR_ACCL_X, CUR_ACCL_Y, NEXT_ACCL_X, NEXT_ACCL_Y) :-
 
 % overall transition constraints
 valid_transition(CUR_POS_X, CUR_POS_Y, CUR_VEL_X, CUR_VEL_Y, CUR_ACCL_X, CUR_ACCL_Y,
-                 NEXT_POS_X, NEXT_POS_Y, NEXT_VEL_X, NEXT_VEL_Y,NEXT_ACCL_X, NEXT_ACCL_Y):-
+                 NEXT_POS_X, NEXT_POS_Y, NEXT_VEL_X, NEXT_VEL_Y,NEXT_ACCL_X, NEXT_ACCL_Y, CARS_LIST):-
     valid_position(CUR_POS_X, CUR_POS_Y, NEXT_POS_X, NEXT_POS_Y),
     displacement(CUR_POS_X, CUR_POS_Y, NEXT_POS_X, NEXT_POS_Y, DISP_X, DISP_Y),
     valid_velocity(DISP_X, DISP_Y, NEXT_VEL_X, NEXT_VEL_Y), 
     valid_acceleration(CUR_VEL_X, CUR_VEL_Y, NEXT_VEL_X, NEXT_VEL_Y, NEXT_ACCL_X, NEXT_ACCL_Y),
-    valid_jerk(CUR_ACCL_X, CUR_ACCL_Y, NEXT_ACCL_X, NEXT_ACCL_Y).
+    valid_jerk(CUR_ACCL_X, CUR_ACCL_Y, NEXT_ACCL_X, NEXT_ACCL_Y),
+    \+ is_car_position(NEXT_POS_X, NEXT_POS_Y,CARS_LIST).
+
+is_car_position(DISP_X,DISP_Y,[[X_CAR,Y_CAR]|CARS_LIST]) :-
+    (DISP_X is X_CAR, 
+    DISP_Y is Y_CAR);
+    is_car_position(DISP_X,DISP_Y,CARS_LIST).
+
+is_car_position(DISP_X,DISP_Y,CARS_LIST):- \+.
